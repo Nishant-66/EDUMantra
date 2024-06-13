f6bd45b2d7cc8bde9f0581bc25519ced06291729
Kumar Nishant <167325502+Nishant-66@users.noreply.github.com>
2024-06-13 23:10:08 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-const dotenv= require('dotenv');
-const app = express();
-dotenv.config();
-const PORT=process.env.PORT ||5000;
-app.listen(PORT,()=>{
-    console.log(`listening on port ${PORT}`);
-})
